9c4a4bbe5f3e0617f5da970c63c1f10335e47449
Samandar1418 <nabiyevs967@gmail.com>
2023-10-25 22:27:01 +0500

    finish

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1 masala
+   
+// 2 masala
+let arr = [ 1,3,4,5,6,7,8,9]
+ function sum (){
+    for (let i = 0; i < arr.length; i += 2) {
+        arr.push(arr[i]);
+      }
+      return arr; 
+ }    
+ console.log(sum ());
+// 3 masala
+
+// 4 masala
+
+// 5 masala
+
+// 6 masala
+// let arr = [ 1,3,4,5,6,7,8,9]
+// sum = () => {
+//      return (arr [arr.findIndex((item)=> item%5==0)]);
+//   }
+// console.log(sum ());
+// 7 masala
+//  let arr = [ 1,3,4,5,6,7,8,9]
+//  function sum  (arr,item) {
+//      return arr.includes(item) ;
+//   }
+// console.log(sum (arr,5));
+// 8 masala
+
+// 9 masala
+
+// 10 masala
